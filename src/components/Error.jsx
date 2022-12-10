89cf499a8537e29bd3a367321bd2e8fdd84f84dd
AMAN <nottherealaman@gmail.com>
2022-12-10 23:52:28 +0530

    add styles

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom'
 
 function Error() {
   return (
-    <div className=''>
+    <div className='flex justify-center items-center bg-blue-400 h-screen '>
         You are LOST !!!!!!!!!!
 
        <Link to={'/'}>
-        <div>
+        <div className='text-red-300 text-underline'>
             Redirect To Home page 
        </div>
        </Link>
